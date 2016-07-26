47519bee4da49fcd3b522191f5c3e5f2b3af3060
Yash Ketkar <yashketkar@hotmail.com>
2016-07-26 16:02:47 -0600

    Add Challenge

---
+++ Counting Valleys/README.md
@@ -0,0 +1,8 @@
+# Problem URL:
+https://www.hackerrank.com/contests/rookierank/challenges/counting-valleys
+
+# Max Score:
+15
+
+# Difficulty:
+Easy
---
+++ Counting Valleys/Solution.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s= sc.next();
+        System.out.print(s);
+    }
+}
